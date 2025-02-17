484e3853c077cf533c6ae5e89b44e9e8ac358f85
monicahwamuhu2 <monicahwamuhu2@gmail.com>
2025-02-17 03:50:18 +0300

    Remove duplicate files and organize structure

--- test_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/chat"
-data = {"text": "I feel stressed today."}
-
-response = requests.post(url, json=data)
-print(response.json())  # Should return chatbot response
